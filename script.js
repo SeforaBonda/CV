a19e751ceb54063a80064a726fa8251843fa8d36
SeforaBonda <116027562+SeforaBonda@users.noreply.github.com>
2022-10-17 21:15:48 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function afiseazaText() {
+    document.querySelector("#experienta1").classList.toggle("element-ascuns");
+    // document query selecotr ne returneaza emenetul, class list ne ofera o lista cu clasele, toggle va adauga o clasa in cazul in carenu este in lista si o va sterge daca este in lista (se aplica doar pt classlist)
+}
+function afiseazaText2() {
+    document.querySelector("#experienta2").classList.toggle("element-ascuns");
+}
